df64a68e1ad9bb0a49ce595f191bf9e145ae351b
Javix33 <javix33@hotmail.com>
2024-02-22 00:48:52 -0600

    final optimizado

--- js/main.js
+++ js/main.js
@@ -16,51 +16,38 @@ function showSlide(n) {
   slides[slideIndex].classList.add("active");
 }
 
-// Cambiar de diapositiva cada 2 segundos
 setInterval(function() {
   changeSlide(1);
 }, 5000);
 
 //final banner  
 //inicio servicios
-//inicio cards
 document.addEventListener('DOMContentLoaded', function () {
   var buttons = document.querySelectorAll('.cardAction');
   buttons.forEach(function (button) {
       button.addEventListener('click', function (event) {
           var additionalInfo = this.nextElementSibling;
-          additionalInfo.classList.toggle('hidden'); // Alternar la clase 'hidden'
-          event.stopPropagation(); // Detener la propagación del evento para evitar conflictos
+          additionalInfo.classList.toggle('hidden');
+          event.stopPropagation();
       });
   });
 });
-//final cards
 //final servicios
 //inicio menu
-// selector
 let menu = document.querySelector('.hamburger');
 
-// Método para alternar el menú
 function toggleMenu(event) {
-    console.log("Toggle menu function is triggered.");
     menu.classList.toggle('is-active');
     let navMenu = document.querySelector(".menuppal");
     navMenu.classList.toggle("is_active");
 }
 
-// Evento para el menú hamburguesa
 menu.addEventListener('click', toggleMenu, false);
 
-// Escuchador de eventos al cargar el DOM
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOMContentLoaded event is triggered.");
-
-    // Event listener para el clic en los enlaces del menú móvil
     let mobileLinks = document.querySelectorAll('.mobile-menu a');
     mobileLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
-            // Cerramos el menú al hacer clic en un enlace del menú móvil
-            console.log("Mobile link clicked.");
             toggleMenu(event);
         });
     });
@@ -69,7 +56,6 @@ document.addEventListener('DOMContentLoaded', function () {
 //final menu
 //button back
 $(document).ready(function() {
-  // Show or hide the sticky footer button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 200) {
       $('.go-top').fadeIn(200);
@@ -78,10 +64,8 @@ $(document).ready(function() {
     }
   });
   
-  // Animate the scroll to top
   $('.go-top').click(function(event) {
     event.preventDefault();
-    
     $('html, body').animate({scrollTop: 0}, 300);
   })
 });
